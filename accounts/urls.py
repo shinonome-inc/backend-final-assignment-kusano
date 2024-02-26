e204dc6fd28b9dd704dba85d8d4d20ef5618f75e
KUS4NO <kusano319@gmail.com>
2024-02-26 16:52:53 +0900

    余分なコードを削除

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -7,6 +7,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
