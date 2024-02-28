9a47109baf7b4184ff72e212ca96304a888957b9
KUS4NO <kusano319@gmail.com>
2024-02-28 11:47:10 +0900

    ログイン・ログアウト機能の実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
-    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
